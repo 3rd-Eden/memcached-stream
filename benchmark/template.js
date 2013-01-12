81154fea0d2334fd49d311180dd7502bc72acd82
3rd-Eden <info@3rd-Eden.com>
2013-01-12 19:37:24 +0100

    [minor] Added benchmarking template

---
+++ benchmark/template.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * Benchmark related modules.
+ */
+var benchmark = require('benchmark')
+  , microtime = require('microtime');
+
+/**
+ * Logger
+ */
+var logger = new(require('devnull'));
+
+(
+  new benchmark.Suite()
+).add('<test1>', function test1() {
+
+}).add('<test2>', function test2() {
+
+}).on('cycle', function cycle(e) {
+  var details = e.target;
+
+  logger.info('Finished benchmarking: "%s"', details.name);
+  logger.metric('Count (%d), Cycles (%d), Elapsed (%d), Hz (%d)'
+    , details.count
+    , details.cycles
+    , details.times.elapsed
+    , details.hz
+  );
+}).on('complete', function completed() {
+  logger.info('Benchmark: "%s" is was the fastest.'
+    , this.filter('fastest').pluck('name')
+  );
+}).run();
