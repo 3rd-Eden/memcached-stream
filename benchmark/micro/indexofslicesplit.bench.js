cf9e022ae5151ed928aa2b557f254a8e497bb79e
3rd-Eden <info@3rd-Eden.com>
2013-01-13 23:55:17 +0100

    [benchmark] Fixed broken benchmark [benchmark] Added more benchmarks

--- benchmark/micro/indexofslicesplit.bench.js
+++ benchmark/micro/indexofslicesplit.bench.js
@@ -14,39 +14,14 @@ var logger = new(require('devnull'))({ timestamp: false, namespacing: 0 });
 /**
  * Preparation code.
  */
-var EventEmitter = require('events').EventEmitter
-  , util = require('util');
-
-// Prototype based initalization
-function Foo() {
-  EventEmitter.apply(this, arguments);
-}
-
-Foo.prototype = new EventEmitter;
-Foo.prototype.constructor = Foo;
-
-// __proto__
-function Bar() {
-
-}
-
-Bar.prototype.__proto__ = EventEmitter.prototype;
-
-// utils.inherit
-function Baz() {
-  EventEmitter.apply(this, arguments);
-}
-
-util.inherits(Baz, EventEmitter);
+var str = 'STAT limit_maxbytes 52428800\r\n';
 
 (
   new benchmark.Suite()
-).add('Standard prototype inherit invocation', function test1() {
-  var x = new Foo();
-}).add('__proto__ inherit invocation', function test2() {
-  var x = new Bar();
-}).add('util.inhertis', function test3() {
-  var x = new Baz();
+).add('str.split( )[1]', function test1() {
+  var version = str.split(' ')[1];
+}).add('str.slice() & indexOf', function test2() {
+  var version = str.slice(str.indexOf('', 8));
 }).on('cycle', function cycle(e) {
   var details = e.target;
 
