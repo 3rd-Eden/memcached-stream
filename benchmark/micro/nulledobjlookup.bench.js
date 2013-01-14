cf9d06787b94e62770f056a862ca2952d4645b7b
3rd-Eden <info@3rd-Eden.com>
2013-01-14 17:15:17 +0100

    [minor] key vs string lookups in a nulled object

---
+++ benchmark/micro/nulledobjlookup.bench.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * Benchmark related modules.
+ */
+var benchmark = require('benchmark')
+  , microtime = require('microtime');
+
+/**
+ * Logger
+ */
+var logger = new(require('devnull'))({ timestamp: false, namespacing: 0 });
+
+/**
+ * Preparation code.
+ */
+var obj = Object.create(null);
+
+obj[1000] = 'CLIENT_ERROR';
+obj['1000'] = 'CLIENT_ERROR';
+
+(
+  new benchmark.Suite()
+).add('string based key', function test1() {
+  var x = obj['1000'];
+}).add('numberic key', function test2() {
+  var x = obj[1000];
+}).on('cycle', function cycle(e) {
+  var details = e.target;
+
+  logger.log('Finished benchmarking: "%s"', details.name);
+  logger.metric('Count (%d), Cycles (%d), Elapsed (%d), Hz (%d)'
+    , details.count
+    , details.cycles
+    , details.times.elapsed
+    , details.hz
+  );
+}).on('complete', function completed() {
+  logger.info('Benchmark: "%s" is was the fastest.'
+    , this.filter('fastest').pluck('name')
+  );
+}).run();
